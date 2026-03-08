a3ea3e03bb48c75d0984039ae24f87766c759931
Darijoo <128596761+Darijoo@users.noreply.github.com>
2026-03-08 23:42:19 +0100

    Navigatie, routing en homepagina met hero-sectie toegevoegd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', // Een leeg pad betekent de standaard homepagina (jouwwebsite.be/)
+    component: Home,
+  },
+];
