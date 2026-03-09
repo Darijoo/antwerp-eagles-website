c854ba7425a92cb96b0621b510e716568e921e12
Darijoo <128596761+Darijoo@users.noreply.github.com>
2026-03-09 13:14:16 +0100

    contact route toegevoegd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Teams } from './teams/teams';
+import { Contact } from './contact/contact';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'teams', // Let op: zonder de schuine streep (/) ervoor
     component: Teams,
+  },
+  {
+    path: 'contact', // Let op: zonder de schuine streep (/) ervoor
+    component: Contact,
   },
 ];
