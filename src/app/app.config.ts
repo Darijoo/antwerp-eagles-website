fd6aee6606987b8161f14f7f86b4611701f769b8
Dario <dario.gilles@outlook.com>
2026-06-19 01:56:05 +0200

    beveiliging update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,21 +10,14 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAKa91f2DbvWU5RCjUgUh6t1wF290bumu8',
-  authDomain: 'antwerp-eagles-d0a87.firebaseapp.com',
-  projectId: 'antwerp-eagles-d0a87',
-  storageBucket: 'antwerp-eagles-d0a87.firebasestorage.app',
-  messagingSenderId: '53557639212',
-  appId: '1:53557639212:web:0fa9393a81868fbd437989',
-};
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), // <-- Moderne optimalisatie voor Zone.js
     provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top' })),
     provideHttpClient(withFetch()),
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore()),
     provideAuth(() => getAuth()),
     provideStorage(() => getStorage()),
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAKa91f2DbvWU5RCjUgUh6t1wF290bumu8',
+    authDomain: 'antwerp-eagles-d0a87.firebaseapp.com',
+    projectId: 'antwerp-eagles-d0a87',
+    storageBucket: 'antwerp-eagles-d0a87.firebasestorage.app',
+    messagingSenderId: '53557639212',
+    appId: '1:53557639212:web:0fa9393a81868fbd437989',
+  }
+};
